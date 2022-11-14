98ba77f2414feee98e6752dd5450d0e7015070ec
Ulya <uleiven@mail.ru>
2022-11-15 01:59:22 +0300

    Задача 6

---
+++ Ex_3/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.Write ("Ввведите число: ");
+int number = int.Parse(Console.ReadLine());
+
+if (number % 2 == 0)
+{
+Console.WriteLine("Число является чётным");
+}
+else
+{
+   Console.WriteLine("Введите другое число"); 
+}
