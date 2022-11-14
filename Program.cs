42e735b9abf6687f23f7b903e3332802a06a58c7
Ulya <uleiven@mail.ru>
2022-11-15 00:45:11 +0300

    Задача 2

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.Write ("Ввведите первое число: ");
+int A=int.Parse(Console.ReadLine());
+
+Console.Write ("Ввведите второе число: ");
+int B=int.Parse(Console.ReadLine());
+
+int max = A;
+
+if (B > max) max = B;
+if (A > max) max = A;
+
+Console.Write("max = ");
+Console.WriteLine(max);
