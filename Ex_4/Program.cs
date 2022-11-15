9b30adc05c687d07f074199c2b447e4f49030791
Ulya <uleiven@mail.ru>
2022-11-15 09:42:00 +0300

    Задача 8

---
+++ Ex_4/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.Write ("Ввведите число N: ");
+int N=int.Parse(Console.ReadLine());
+int number=1;
+
+while(number<N)
+{
+    if(number%2==0)
+    {
+        Console.Write($"{number}");
+    }
+    number = number + 1;
+}
